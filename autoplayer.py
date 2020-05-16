d5ab1d6d53b46e242a411c50360307892525a09f
Damjan <damjandimitrov1@gmail.com>
2020-05-16 20:03:05 +0200

    Move Player2048 class to separate file

--- autoplayer.py
+++ autoplayer.py
@@ -1,14 +1,5 @@
-from selenium import webdriver
-
-
-class Player2048:
-    def __init__(self):
-        self.browser = webdriver.Chrome()
-        self.browser.get('https://play2048.co/')
-
+from player2048 import Player2048
 
 player = Player2048()
 tile_container = player.browser.find_element_by_class_name('tile-container')
-divs = tile_container.find_elements_by_tag_name('div')
-divHTML = [div.find_element_by_xpath('.').get_attribute('outerHTML') for div in divs]
-player.browser.quit()
+player.close()
