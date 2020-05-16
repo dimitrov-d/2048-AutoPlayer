a034b6e058c98034cc4ad3a10a6f16a309b2f514
Damjan <damjandimitrov1@gmail.com>
2020-05-16 21:24:41 +0200

    Loop through rows and columns and search for equal tiles

--- autoplayer.py
+++ autoplayer.py
@@ -1,18 +1,41 @@
+import random
 from time import sleep
+
+from selenium.webdriver.common.keys import Keys
 
 from player2048 import Player2048
 
 player = Player2048()
 sleep(2)
+for i in range(13):
+    player.html.send_keys(random.choice([Keys.UP, Keys.DOWN, Keys.LEFT, Keys.RIGHT]))
+    sleep(0.3)
 tile_container = player.browser.find_element_by_class_name('tile-container')
 positions, numbers = player.parse_string_regex(tile_container.get_attribute('innerHTML'))
 
-print(positions, numbers)
-# grid = [[pos for pos in positions if pos[0]=='1'], [pos for pos in positions if pos[0]=='2'],
-#         [pos for pos in positions if pos[0]=='3'], [pos for pos in positions if pos[0]=='4']]
-grid = {}
+grid = [[pos for pos in positions if pos[0] == '1'], [pos for pos in positions if pos[0] == '2'],
+        [pos for pos in positions if pos[0] == '3'], [pos for pos in positions if pos[0] == '4']]
+
+tile_mapping = {}
 for i in range(len(positions)):
-    grid[positions[i]] = numbers[i]
+    tile_mapping[positions[i]] = numbers[i]
 
 print(grid)
+print(tile_mapping)
+
+for column in grid:
+    if len(column) > 1:
+        for tile in range(1, len(column) - 1):
+            if (int(tile_mapping[column[tile]]) == int(tile_mapping[column[tile - 1]])) and column[tile] != column[tile-1]:
+                print('col equal')
+
+from itertools import zip_longest
+rows = r = [list(filter(None,i)) for i in zip_longest(*grid)]
+
+for row in rows:
+    for tile in range(1, len(row) - 1):
+        if (int(tile_mapping[row[tile]]) == int(tile_mapping[row[tile - 1]])) and row[tile] != row[tile - 1]:
+            print('row equal')
+
+
 # player.close()
