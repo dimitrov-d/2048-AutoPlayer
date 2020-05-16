cf401460b4e752e5d19d8d4559f363aa02b7793b
Damjan <damjandimitrov1@gmail.com>
2020-05-16 19:27:55 +0200

    Gather elements from HTML code

--- autoplayer.py
+++ autoplayer.py
@@ -8,3 +8,7 @@ class Player2048:
 
 
 player = Player2048()
+tile_container = player.browser.find_element_by_class_name('tile-container')
+divs = tile_container.find_elements_by_tag_name('div')
+divHTML = [div.find_element_by_xpath('.').get_attribute('outerHTML') for div in divs]
+player.browser.quit()
