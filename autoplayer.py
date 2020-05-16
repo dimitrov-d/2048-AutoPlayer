3028c35409bd2cddacec692dfc6ec73768e41d84
Damjan <damjandimitrov1@gmail.com>
2020-05-16 19:19:08 +0200

    Added class with constructor

---
+++ autoplayer.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+
+class Player2048:
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+        self.browser.get('https://play2048.co/')
+
+
+player = Player2048()
